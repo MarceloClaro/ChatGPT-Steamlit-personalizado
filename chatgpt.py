c47d28ce3fa3cbde98d1b28e3e8db7e07004158a
MARCELO CLARO LARANJEIRA <58664974+MarceloClaro@users.noreply.github.com>
2023-04-07 18:28:03 -0300

    Update chatgpt.py

--- chatgpt.py
+++ chatgpt.py
@@ -1,41 +1,40 @@
-import os
-import streamlit as st
 import openai
+import streamlit as st
 
-# Instala o pacote openai
-os.system('pip install openai')
+# Defina sua chave de API
+openai.api_key = "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"
 
-# Define a chave da API da OpenAI
-openai.api_key = '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'
+# Defina o modelo que será usado para gerar as respostas
+model_engine = "text-davinci-002"
 
-# Define as mensagens iniciais
-messages = [
-    {"role": "system", "content": "Você é um especialista em prompts de artigos científicos com citações e referências."},
-]
-
-# Define o título da página
-st.title('Chat com OpenAI GPT-3')
-
-# Cria a caixa de texto para o usuário digitar sua mensagem
-user_input = st.text_input('User :')
-
-# Se o usuário digitou uma mensagem, adiciona a mensagem à lista de mensagens
-if user_input:
-    messages.append({"role": "user", "content": user_input})
-    
-    # Envia as mensagens para o modelo GPT-3 da OpenAI e recebe a resposta
-    chat = openai.Completion.create(
-        model="text-davinci-002",
-        prompt=" ".join([message["content"] for message in messages]),
-        max_tokens=4000, # Definindo o valor máximo de tokens para 4000
-        temperature=0.5, # Define a "criatividade" da resposta
+# Define a função para gerar uma resposta com base em uma pergunta
+def generate_answer(question):
+    prompt = f"Eu gostaria de saber {question}. A resposta é:"
+    response = openai.Completion.create(
+        engine=model_engine,
+        prompt=prompt,
+        max_tokens=1024,
+        n=1,
+        stop=None,
+        temperature=0.7,
     )
-    
-    # Extrai a resposta do objeto retornado pelo modelo
-    reply = chat.choices[0].text
-    
-    # Adiciona a resposta à lista de mensagens
-    messages.append({"role": "assistant", "content": reply})
+    return response.choices[0].text.strip()
 
-# Mostra as mensagens em uma caixa de texto grande
-st.text_area("ChatGPT:", value="\n".join([f'{msg["role"]}: {msg["content"]}' for msg in messages]), height=200)
+# Cria a interface do Streamlit
+def main():
+    st.title("Gerador de respostas usando OpenAI")
+
+    # Obtém a pergunta do usuário
+    question = st.text_input("Faça uma pergunta:")
+    if not question:
+        return
+
+    # Gera a resposta com base na pergunta
+    with st.spinner("Gerando resposta..."):
+        answer = generate_answer(question)
+
+    # Mostra a resposta para o usuário
+    st.write(answer)
+
+if __name__ == "__main__":
+    main()
