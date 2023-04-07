aebcb55e7bba7bac7ac45ae1c02f4c6c8b1cb74e
MARCELO CLARO LARANJEIRA <58664974+MarceloClaro@users.noreply.github.com>
2023-04-07 19:06:44 -0300

    Update chatgpt.py

--- chatgpt.py
+++ chatgpt.py
@@ -13,7 +13,7 @@ def generate_answer(question):
     response = openai.Completion.create(
         engine=model_engine,
         prompt=prompt,
-        max_tokens=2000,
+        max_tokens=1024,
         n=1,
         stop=None,
         temperature=0.7,
