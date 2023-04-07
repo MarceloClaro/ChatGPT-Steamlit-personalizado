28cce5a381c32047f97f530d832e31b070d7368f
MARCELO CLARO LARANJEIRA <58664974+MarceloClaro@users.noreply.github.com>
2023-04-07 20:06:17 -0300

    Update chatgpt.py

--- chatgpt.py
+++ chatgpt.py
@@ -20,7 +20,7 @@ submit_button = form.form_submit_button(label='Enviar')
 if submit_button:
     messages.append({"role": "user", "content": message})
     chat = openai.Completion.create(
-        engine="davinci",
+        engine="gpt-3.5-turbo",
         prompt=f"Conversation with the chatbot:\nUser: {message}\nBot:",
         temperature=0.7,
         max_tokens=1024,
