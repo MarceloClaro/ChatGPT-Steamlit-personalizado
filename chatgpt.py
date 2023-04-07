26b0cb3e5d1638708a69718313cbe4d78881b225
MARCELO CLARO LARANJEIRA <58664974+MarceloClaro@users.noreply.github.com>
2023-04-07 18:39:47 -0300

    Update chatgpt.py

--- chatgpt.py
+++ chatgpt.py
@@ -2,7 +2,7 @@ import openai
 import streamlit as st
 
 # Defina sua chave de API
-openai.api_key = "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"
+openai.api_key = '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'
 
 # Defina o modelo que será usado para gerar as respostas
 model_engine = "text-davinci-002"
@@ -13,17 +13,17 @@ def generate_answer(question):
     response = openai.Completion.create(
         engine=model_engine,
         prompt=prompt,
-        max_tokens=3000,
+        max_tokens=1024,
         n=1,
         stop=None,
-        temperature=0.5,
+        temperature=0.7,
     )
     return response.choices[0].text.strip()
 
 # Cria a interface do Streamlit
 def main():
-    st.title("Gerador de respostas usando OpenAI - Marcelo Claro")
-    
+    st.title("Gerador de respostas usando OpenAI")
+
     # Obtém a pergunta do usuário
     question = st.text_input("Faça uma pergunta:")
     if not question:
